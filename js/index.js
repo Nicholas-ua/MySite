caef97cb6fe0faf5e1a47e5357eb613579bcd163
Nicholas <m.savych@gmail.com>
2017-11-26 13:25:19 +0000

    Add main and footer

--- js/index.js
+++ js/index.js
@@ -26,3 +26,12 @@ function sendForm(e) {
         $('#msg').html('Sorry, there is an error!');
     });
 }
+
+$(document).bind( 'mousewheel', function (e) { 
+    var nt = $(document.body).scrollTop()-(e.deltaY*e.deltaFactor*100); 
+    e.preventDefault(); 
+    e.stopPropagation(); 
+    $(document.body).stop().animate( { 
+         scrollTop : nt 
+     } , 500 , 'easeInOutCubic' );  
+} )
